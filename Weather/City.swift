b2ad4f9190f82c1e9ef16316d0a757701bcf6e47
MariannaFomina <Fomina.Marianna.V@gmail.com>
2022-11-23 07:34:34 +0300

    weather

---
+++ Weather/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  Weather
+//
+//  Created by user on 23.11.2022.
+//
+
+import Foundation
+
+struct City {
+    
+    var name: String
+    var temp_c: Double
+    var country: String
+    
+}
---
+++ Weather/Config.swift
@@ -0,0 +1,10 @@
+//
+//  Config.swift
+//  Weather
+//
+//  Created by user on 23.11.2022.
+//
+
+import Foundation
+
+let apiKey = "03720ffda9ab47c088314933222311"
